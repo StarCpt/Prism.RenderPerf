d1f6878bd3f5e63b16de73ebb0c814e51d3fe7b1
StarCpt <86216339+StarCpt@users.noreply.github.com>
2025-11-23 17:42:01 -0800

    organization

--- Prism.RenderPerf/Properties/IgnoreAccessChecksToAttribute.cs
+++ Prism.RenderPerf/Properties/IgnoreAccessChecksToAttribute.cs
@@ -1,4 +1,9 @@
-﻿namespace System.Runtime.CompilerServices
+﻿[assembly: System.Runtime.CompilerServices.IgnoresAccessChecksTo("Sandbox.Game")]
+[assembly: System.Runtime.CompilerServices.IgnoresAccessChecksTo("VRage.Render")]
+[assembly: System.Runtime.CompilerServices.IgnoresAccessChecksTo("VRage.Render11")]
+[assembly: System.Runtime.CompilerServices.IgnoresAccessChecksTo("VRage.Platform.Windows")]
+
+namespace System.Runtime.CompilerServices
 {
     [AttributeUsage(AttributeTargets.Assembly, AllowMultiple = true)]
     internal sealed class IgnoresAccessChecksToAttribute : Attribute
